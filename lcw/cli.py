6b06a6f5ac08f46bbcb13186b25e3116bcb1fc61
Thomas Levine <_@thomaslevine.com>
2015-08-17 02:42:25 -0400

    multiple files

--- lcw/cli.py
+++ lcw/cli.py
@@ -7,13 +7,11 @@ from . import lcw
 argparser = argparse.ArgumentParser('lcw',
     formatter_class = argparse.ArgumentDefaultsHelpFormatter,
     description = 'Estimate how many lines are in a file.')
-argparser.add_argument('file', type = argparse.FileType('rb'))
+argparser.add_argument('file', type = argparse.FileType('rb'), nargs = '+')
 argparser.add_argument('--sample-size', '-n', type = int, default = 1000, dest = 'n',
                        help = 'How many pages to count')
 argparser.add_argument('--page-size', '-p', type = int, default = 2 ** 14,
                        help = 'Size of an observation')
-argparser.add_argument('--just-ml', '-j', action = 'store_true',
-                       help = 'Only print the maximum likelihood estimate')
 argparser.add_argument('--pattern', '-e', default = b'\n',
                        type = lambda x: x.encode('utf-8'),
                        help = 'The pattern to match')
@@ -26,24 +24,27 @@ def main():
         sys.stderr.write('Sample size must be at least one.\n')
         return 1
 
-
-    if os.stat(args.file.name).st_size > 1.5 * args.page_size:
-        stats = lcw.count(args.file, n = args.n, page_size = args.page_size,
-                          pattern = args.pattern, regex = args.regex)
-        template = '%(ml)d ± %(radius)d occurrences (99%% confidence)\n'
-    else:
-        template = '%(ml)d occurrences (100%% confidence)\n'
-        if args.regex:
-            ml = len(re.findall(args.pattern, args.file.read()))
+    max_filename_length = max(len(fp.name) for fp in args.file)
+    for fp in args.file:
+        if os.stat(fp.name).st_size > 1.5 * args.page_size:
+            stats = lcw.count(fp, n = args.n, page_size = args.page_size,
+                              pattern = args.pattern, regex = args.regex)
+            template = '%(fn)s%(space)s%(ml)d ± %(radius)d occurrences (99%% confidence)\n'
         else:
-            ml = args.file.read().count(args.pattern)
-        stats = {
-            'ml': ml
-        }
+            template = '%(fn)s%(space)s%(ml)d occurrences (100%% confidence)\n'
+            if args.regex:
+                ml = len(re.findall(args.pattern, fp.read()))
+            else:
+                ml = fp.read().count(args.pattern)
+            stats = {
+                'ml': ml,
+            }
 
-    if args.just_ml:
-        sys.stdout.write('%d\n' % stats['ml'])
-    else:
+        stats.update({
+            'fn': fp.name,
+            'space': (max_filename_length + 1 - len(fp.name)) * ' ',
+        })
+
         sys.stdout.write(template % stats)
 
 if __name__ == '__main__':
