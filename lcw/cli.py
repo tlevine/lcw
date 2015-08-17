0f9d7369e637acb337485315a4772da7ee875f5b
Thomas Levine <_@thomaslevine.com>
2015-08-16 21:34:29 -0400

    remove features

--- lcw/cli.py
+++ lcw/cli.py
@@ -8,17 +8,12 @@ argparser = argparse.ArgumentParser('Estimate how many lines are in a file.')
 argparser.add_argument('file', type = argparse.FileType('rb'))
 argparser.add_argument('--sample-size', '-n', type = int, default = 100, dest = 'n',
                        help = 'Number of lines to sample for the estimate')
-argparser.add_argument('--confidence', '-c', type = int, default = .99,
-                       help = 'Confidence level')
 
 
 def main():
     args = argparser.parse_args()
     if args.n < 1:
         sys.stderr.write('Sample size must be at least one.\n')
-        return 1
-    elif not (0 < args.confidence < 1):
-        sys.stderr.write('Confidence level must be between 0 and 1, exclusive.\n')
         return 1
 
     filesize = os.stat(args.file.name).st_size
